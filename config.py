27af79524dcf2e86c8b8ba9e440760e6d47747f5
deltabot69 <115163906+deltabot69@users.noreply.github.com>
2023-05-07 23:35:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6143332281:AAHxuBpyNe0Gkq0hYTLoZ6M-Ut4NECPfgEA")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "12637041"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "fc9943e89e1739f9c29140b360ef80cc")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1966376217 738010143"))
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
